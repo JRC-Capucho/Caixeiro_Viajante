6fc72c9846d526557aaf440e550029cc621ce30b
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2023-04-13 21:26:42 -0300

    New form in valia

--- caixeiro.py
+++ caixeiro.py
@@ -1,24 +1,25 @@
 from random import shuffle, randint, randrange
 import copy as cp
 
+
 class Caixeiro():
 
-    def gera_ambiente(self,minimo,maximo,tam):
-        
+    def gera_ambiente(self, minimo, maximo, tam):
+
         matriz = []
         for i in range(tam):
             linha = []
             for j in range(tam):
-                if i==j:
+                if i == j:
                     linha.append(0)
                 else:
-                    linha.append(randint(minimo,maximo))
+                    linha.append(randint(minimo, maximo))
             matriz.append(linha)
-        
+
         return matriz
 
-    def solucao_inicial(self,tam):
-        
+    def solucao_inicial(self, tam):
+
         solucao = []
         for i in range(tam):
             solucao.append(i)
@@ -26,43 +27,41 @@ class Caixeiro():
 
         return solucao
 
-    def avalia_solucao(self,solucao,matriz1,matriz2,matriz3,tam):
-        
+    def avalia_solucao(self, solucao, matriz1, matriz2, tam):
         valor = 0
         for i in range(tam-1):
-            valor += matriz1[solucao[i]][solucao[i+1]] + 1/matriz2[solucao[i]][solucao[i+1]] + matriz3[solucao[i]][solucao[i+1]]
-                     
-        valor += matriz1[solucao[tam-1]][solucao[0]] + 1/matriz2[solucao[tam-1]][solucao[0]] + matriz3[solucao[tam-1]][solucao[0]]
-        
+            valor += matriz1[solucao[i]][solucao[i+1]] * \
+                1/matriz2[solucao[i]][solucao[i+1]]
+        valor += matriz1[solucao[tam-1]][solucao[0]] * \
+            1/matriz2[solucao[tam-1]][solucao[0]]
+
         return valor
 
-
-    def encosta(self,solucao,matriz1,matriz2,matriz3,valor):
+    def encosta(self, solucao, matriz1, matriz2, valor):
         atual = cp.deepcopy(solucao)
         va = valor
-        
+
         while True:
-            novo, vn = self.sucessores_enc(atual,matriz1,matriz2,matriz3,va)
-    
-            if vn>=va:
-                    return atual, va
+            novo, vn = self.sucessores_enc(atual, matriz1, matriz2, va)
+
+            if vn >= va:
+                return atual, va
             else:
                 atual = novo
                 va = vn
 
-
-    def encosta_alterada(self,solucao,matriz1,matriz2,matriz3,valor):
+    def encosta_alterada(self, solucao, matriz1, matriz2, valor):
         atual = cp.deepcopy(solucao)
         va = valor
         tmax = len(atual)
         t = 1
-        
+
         while True:
-            novo, vn = self.sucessores_enc(atual,matriz1,matriz2,matriz3,va)
-    
-            if vn>=va:
-                #subida de encosta alterada
-                if t>tmax:
+            novo, vn = self.sucessores_enc(atual, matriz1, matriz2, va)
+
+            if vn >= va:
+                # subida de encosta alterada
+                if t > tmax:
                     return atual, va, t
                 else:
                     t += 1
@@ -71,31 +70,28 @@ class Caixeiro():
                 va = vn
                 t = 1
 
-
-
-    def sucessores_enc(self,solucao,matriz1,matriz2,matriz3,valor):
+    def sucessores_enc(self, solucao, matriz1, matriz2, valor):
         melhor = cp.deepcopy(solucao)
         vm = valor
-        
-        c = randrange(0,len(solucao))
-        
+
+        c = randrange(0, len(solucao))
+
         for i in range(len(solucao)):
             aux = cp.deepcopy(solucao)
-            x      = aux[c]
+            x = aux[c]
             aux[c] = aux[i]
             aux[i] = x
-            
-            v_aux = self.avalia_solucao(aux,matriz1,matriz2,matriz3,len(solucao))
-            
-            if v_aux<vm:
+
+            v_aux = self.avalia_solucao(aux, matriz1, matriz2, len(solucao))
+
+            if v_aux < vm:
                 melhor = aux
                 vm = v_aux
-        
-        return melhor, vm
 
+        return melhor, vm
 
     # def ganho():
         # ganho = 100*(vi-vf)/vi
         # for i in N:
-            # ganho
+        # ganho
         # ganho / N
--- widget.py
+++ widget.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QWidget
 from teste import Ui_Form
 from caixeiro import Caixeiro
 
+
 class Widget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
@@ -13,46 +14,48 @@ class Widget(QWidget, Ui_Form):
         if self.subidaDeEncosta.isChecked():
             tam = int(self.lineEdit.text())
 
-            matriz_one = self.caixeiro.gera_ambiente(5,20,tam)
-            matriz_two =  self.caixeiro.gera_ambiente(5,10,tam)
-            matriz_three =  self.caixeiro.gera_ambiente(1,10,tam)
+            matriz_one = self.caixeiro.gera_ambiente(5, 20, tam)
+            matriz_two = self.caixeiro.gera_ambiente(5, 10, tam)
             si = self.caixeiro.solucao_inicial(tam)
-            vi = self.caixeiro.avalia_solucao(si,matriz_one,matriz_two,matriz_three,tam)
+            vi = self.caixeiro.avalia_solucao(si, matriz_one, matriz_two, tam)
 
-            sf, vf = self.caixeiro.encosta(si,matriz_one,matriz_two,matriz_three,vi)
+            sf, vf = self.caixeiro.encosta(si, matriz_one, matriz_two, vi)
 
-            stringFinal = "Solução Inicial\n" + str(si) + "\nValor Inicial\n" + str(vi) + "\nSolução Final\n" +str(sf) +"\nValor Final\n" + str(vf);
+            stringFinal = "Solução Inicial\n" + str(si) + "\nValor Inicial\n" + str(
+                vi) + "\nSolução Final\n" + str(sf) + "\nValor Final\n" + str(vf)
             self.solucaoFinal.setPlainText(stringFinal)
             print("Subida de encosta ")
 
         elif self.subidaDeEncostaAlterada.isChecked():
             tam = int(self.lineEdit.text())
-            matriz_one = self.caixeiro.gera_ambiente(5,10,tam)
-            matriz_two =  self.caixeiro.gera_ambiente(5,10,tam)
-            matriz_three =  self.caixeiro.gera_ambiente(1,3,tam)
-            si = self.caixeiro.solucao_inicial(tam)
-            vi = self.caixeiro.avalia_solucao(si,matriz_one,matriz_two,matriz_three,tam)
+            matriz_one = self.caixeiro.gera_ambiente(5, 10, tam)
+            matriz_two = self.caixeiro.gera_ambiente(5, 10, tam)
 
-            sf, vf, t= self.caixeiro.encosta_alterada(si,matriz_one,matriz_two,matriz_three,vi)
+            si = self.caixeiro.solucao_inicial(tam)
+            vi = self.caixeiro.avalia_solucao(si, matriz_one, matriz_two, tam)
+
+            sf, vf, t = self.caixeiro.encosta_alterada(
+                si, matriz_one, matriz_two, vi)
 
             if not self.tentativas.isChecked():
-                stringFinal = "Solução Inicial\n" + str(si) + "\nValor Inicial\n" + str(vi) + "\nSolução Final\n" +str(sf) +"\nValor Final\n" + str(vf);
+                stringFinal = "Solução Inicial\n" + str(si) + "\nValor Inicial\n" + str(
+                    vi) + "\nSolução Final\n" + str(sf) + "\nValor Final\n" + str(vf)
             else:
-                stringFinal = "Solução Inicial\n" + str(si) + "\nValor Inicial\n" + str(vi) + "\nSolução Final\n" +str(sf) +"\nValor Final\n" + str(vf) + "\nTentativas\n" +str(t);
+                stringFinal = "Solução Inicial\n" + str(si) + "\nValor Inicial\n" + str(
+                    vi) + "\nSolução Final\n" + str(sf) + "\nValor Final\n" + str(vf) + "\nTentativas\n" + str(t)
 
             self.solucaoFinal.setPlainText(stringFinal)
             print("Subida de encosta alterada")
 
         else:
             tam = int(self.lineEdit.text())
-            matriz_one = self.caixeiro.gera_ambiente(5,10,tam)
-            matriz_two =  self.caixeiro.gera_ambiente(5,10,tam)
-            matriz_three =  self.caixeiro.gera_ambiente(1,3,tam)
+            matriz_one = self.caixeiro.gera_ambiente(7, 36, tam)
+            matriz_two = self.caixeiro.gera_ambiente(1, 10, tam)
+
             si = self.caixeiro.solucao_inicial(tam)
-            sf = self.caixeiro.avalia_solucao(si,matriz_one,matriz_two,matriz_three,tam)
-            stringFinal = "Solução Inicial\n" + str(si) + "\nValor Inicial\n" + str(sf)
+            sf = self.caixeiro.avalia_solucao(si, matriz_one, matriz_two, tam)
+            stringFinal = "Solução Inicial\n" + \
+                str(si) + "\nValor Inicial\n" + str(sf)
             self.solucaoFinal.setPlainText(stringFinal)
-        
 
         print("Clicked Button")
-    
